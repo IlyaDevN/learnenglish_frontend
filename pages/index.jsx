0b4b06307dcd0fa63b6c8d8ef721e90edd8351bb
IlyaDevN <nevedrov.87@gmail.com>
2023-11-15 20:19:45 +0200

    refactor: Add sentence downloading from the server

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import bgSrc from "./bg.jpg";
-import { Source_Sans_3 } from "next/font/google";
 import clsx from "clsx";
+import { Source_Sans_3 } from "next/font/google";
 import { UiButton } from "../components/ui/UiButton";
 import { ContentField } from "../components/ui/ContentField";
 import { InputField } from "../components/ui/InputField";
-import { useEffect, useState, useRef } from "react";
-import { db } from "../DB";
+import { useState, useRef, useEffect } from "react";
 
 const sourceSans3 = Source_Sans_3({
   subsets: ["latin", "cyrillic"],
@@ -14,16 +13,32 @@ const sourceSans3 = Source_Sans_3({
 });
 
 export default function HomePage() {
-  const [sentences, setSentences] = useState(db);
-  const [randomNumber, setRandomNumber] = useState(() => getRandomNumber(0, sentences.length));
+  const [initialData, setInitialData] = useState();
+  const [sentences, setSentences] = useState([]);
+  const [randomNumber, setRandomNumber] = useState();
   const [currentAnswer, setCurrentAnswer] = useState();
-  const count = useRef(sentences.length);
+  const [isDataLoading, setIsDataLoading] = useState(false);
+  const count = useRef();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://ilyadevn.github.io/JsonApi/data.json");
+      const data = await response.json();
+      setInitialData(data.db);
+      setSentences([...sentences, ...data.db]);
+      setRandomNumber(getRandomNumber(0, data.db.length));
+      count.current = data.db.length;
+      setIsDataLoading(true);
+    };
+
+    fetchData();
+  }, []);
 
   function handleNextSentence() {
     const sentencesCopy = sentences.slice();
     sentencesCopy.splice(randomNumber, 1);
     setSentences(sentencesCopy);
-	count.current -= 1;
+    count.current -= 1;
     setRandomNumber(getRandomNumber(0, count.current));
   }
 
@@ -32,11 +47,11 @@ export default function HomePage() {
   }
 
   function handleReset() {
-    setSentences(db);
-	count.current = db.length;
-	setRandomNumber(getRandomNumber(0, count.current));
+    setSentences(initialData);
+    count.current = initialData.length;
+    setRandomNumber(getRandomNumber(0, count.current));
   }
-  
+
   function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -56,10 +71,11 @@ export default function HomePage() {
           "w-full bg-orange-100 border-4 border-s-gray-100 rounded-2xl px-3.5 py-3.5 flex flex-col gap-4 bg-opacity-80",
         )}
       >
-        <ContentField suppressHydrationWarning={false}>
-          {sentences[randomNumber]
-            ? sentences[randomNumber].question
-            : "The end :)"}
+        <ContentField>
+          {isDataLoading &&
+            (sentences.length
+              ? sentences[randomNumber].question
+              : "The lesson is over")}
         </ContentField>
         <ContentField>
           {currentAnswer || "Нажмите показать перевод"}
