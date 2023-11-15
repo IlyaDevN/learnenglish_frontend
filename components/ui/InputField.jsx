debccfd1614e0cc4c28cca53eac53380c73b8a33
IlyaDevN <nevedrov.87@gmail.com>
2023-11-15 12:19:05 +0200

    refactor: Make placeholder transparent on focus

--- components/ui/InputField.jsx
+++ components/ui/InputField.jsx
@@ -4,7 +4,7 @@ export function InputField({ className }) {
   return (
     <textarea
       className={clsx(
-        "text-base text-yellow-900 px-3.5 py-3 resize-none outline-none border-2 rounded-lg border-yellow-900 bg-white bg-opacity-50 shadow-lg placeholder-yellow-900",
+        "text-base text-yellow-900 px-3.5 py-3 resize-none outline-none border-2 rounded-lg border-yellow-900 bg-white bg-opacity-50 shadow-lg placeholder-yellow-900 placeholder:focus:text-transparent",
         className,
       )}
 	  placeholder="Напишите перевод"
