0b5e7f694e8f335215742be36ceb838b14c0fe51
IlyaDevN <nevedrov.87@gmail.com>
2024-01-08 13:32:02 +0200

    refactor: Add greating to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,12 @@ import { useContext, useEffect } from "react";
 import { ContentContext } from "../context";
 import { useRouter } from "next/router";
 import { Cookies } from "react-cookie";
+import { Source_Sans_3 } from "next/font/google";
+
+const sourceSans3 = Source_Sans_3({
+	subsets: ["latin", "cyrillic"],
+	weight: ["700", "900"],
+});
 
 export default function HomePage() {
   const { isAuth, setIsAuth, currentUser, setCurrentUser } = useContext(ContentContext);
@@ -21,16 +27,14 @@ export default function HomePage() {
 
   return (
     <div className="px-4 py-7">
-      {/* <div
+      <div
         className={clsx(
           sourceSans3.className,
           "w-full bg-orange-100 border-4 border-s-gray-100 rounded-2xl px-3.5 py-8 bg-opacity-80",
         )}
       >
-        <div className="flex justify-evenly mb-7">
-			
-        </div>
-      </div> */}
+		<h1 className="text-2xl font-black text-yellow-900 uppercase text-center">Добро пожаловать</h1>
+      </div>
     </div>
   );
 }
