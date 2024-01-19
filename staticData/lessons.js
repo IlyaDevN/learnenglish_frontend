7b42a5935519bce058e59ec7e992d369c05ce302
IlyaDevN <nevedrov.87@gmail.com>
2024-01-19 19:36:11 +0200

    refactor: Correct lessons address in lessons.js

--- staticData/lessons.js
+++ staticData/lessons.js
@@ -1,4 +1,4 @@
 export const LESSONS = {
-	1: "https://ilyadevn.github.io/JsonApi/lesson_1.json",
-	2: "https://ilyadevn.github.io/JsonApi/lesson_2.json"
+	1: "https://ilyadevn.github.io/JsonApi/english_galaxy/lesson_1.json",
+	2: "https://ilyadevn.github.io/JsonApi/english_galaxy/lesson_2.json"
   }
