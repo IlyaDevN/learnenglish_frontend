0f4982d6e0c348d38533e214dcba1947f5d439a0
IlyaDevN <nevedrov.87@gmail.com>
2024-05-07 22:23:18 +0300

    refactor: Add a lesson_2 to my_sentences

--- staticData/my_sentences.js
+++ staticData/my_sentences.js
@@ -3,5 +3,10 @@ export const my_sentences = [
 		id: 1,
 		name: "Урок 1",
 		address: "https://ilyadevn.github.io/JsonApi/my_sentences/lesson_1.json",
+	},
+	{	
+		id: 1,
+		name: "Урок 2",
+		address: "https://ilyadevn.github.io/JsonApi/my_sentences/lesson_2.json",
 	}
 ]
