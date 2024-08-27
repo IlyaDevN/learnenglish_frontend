d84f34033f83373f29275219b34c66b5e650c01b
IlyaDevN <nevedrov.87@gmail.com>
2024-08-27 15:48:34 +0300

    refactor: Turn off "words" in menu

--- staticData/index.js
+++ staticData/index.js
@@ -1,11 +1,11 @@
 import { english_galaxy } from "./english_galaxy";
 import { orange_playlist } from "./orange_playlist";
 import { my_sentences } from "./my_sentences";
-import { words } from "./7000_words";
+// import { words } from "./7000_words";
 
 export const TASKS = {
   english_galaxy,
   orange_playlist,
   my_sentences,
-  words
+//   words
 };
