67fdc7690f7d1674fc90b983d506994db6dbd0fc
IlyaDevN <nevedrov.87@gmail.com>
2024-09-17 14:49:15 +0300

    refactor: Change Counter color

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,7 +1,7 @@
 export default function Counter({value}) {
 
 	return (
-		<div className="w-[38px] text-center align-middle leading-[30px] bg-blue-500 border-4 rounded-full border-yellow-400 text-xl text-white">
+		<div className="w-[38px] text-center align-middle leading-[30px] bg-white border-4 rounded-full border-yellow-400 text-xl text-yellow-900">
 			{value}
 		</div>
 	)
