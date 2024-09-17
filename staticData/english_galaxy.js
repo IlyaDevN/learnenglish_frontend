4c0035554179448f8487d58b0a23ca7c1d362540
IlyaDevN <nevedrov.87@gmail.com>
2024-09-17 17:00:48 +0300

    refactor: Modify english_galaxy.js

--- staticData/english_galaxy.js
+++ staticData/english_galaxy.js
@@ -1,4 +1,268 @@
+export const levels = [
+	{
+		id: 1,
+		name: "A0"
+	},
+	{
+		id: 2,
+		name: "A1"
+	},
+	{
+		id: 3,
+		name: "A2"
+	},
+	{
+		id: 4,
+		name: "B1"
+	},
+	{
+		id: 5,
+		name: "B2"
+	},
+	{
+		id: 6,
+		name: "C1"
+	}
+];
+
 export const english_galaxy = [
+	{	
+		id: 1,
+		level: "A0",
+		name: "Урок 1",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_1.json",
+	},
+	{	
+		id: 2,
+		level: "A0",
+		name: "Урок 2",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_2.json"
+	},
+	{	
+		id: 3,
+		level: "A0",
+		name: "Урок 3",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_3.json"
+	},
+	{	
+		id: 4,
+		level: "A0",
+		name: "Урок 4",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_4.json"
+	},
+	{	
+		id: 5,
+		level: "A0",
+		name: "Урок 5",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_5.json"
+	},
+	{	
+		id: 6,
+		level: "A0",
+		name: "Урок 6",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_6.json"
+	},
+	{	
+		id: 7,
+		level: "A0",
+		name: "Урок 7",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_7.json"
+	},
+	{	
+		id: 8,
+		level: "A0",
+		name: "Урок 8",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_8.json"
+	},
+	{	
+		id: 9,
+		level: "A0",
+		name: "Урок 9",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_9.json"
+	},
+	{	
+		id: 10,
+		level: "A0",
+		name: "Урок 10",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_10.json"
+	},
+	{	
+		id: 11,
+		level: "A0",
+		name: "Урок 11",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_11.json"
+	},
+	{	
+		id: 12,
+		level: "A0",
+		name: "Урок 12",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_12.json"
+	},
+	{	
+		id: 13,
+		level: "A0",
+		name: "Урок 13",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_13.json"
+	},
+	{	
+		id: 14,
+		level: "A0",
+		name: "Урок 14",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_14.json"
+	},
+	{	
+		id: 15,
+		level: "A0",
+		name: "Урок 15",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_15.json"
+	},
+	{	
+		id: 16,
+		level: "A0",
+		name: "Урок 16",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_16.json"
+	},
+	{	
+		id: 17,
+		level: "A0",
+		name: "Урок 17",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_17.json"
+	},
+	{	
+		id: 18,
+		level: "A0",
+		name: "Урок 18",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_18.json"
+	},
+	{	
+		id: 19,
+		level: "A0",
+		name: "Урок 19",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_19.json"
+	},
+	{	
+		id: 20,
+		level: "A0",
+		name: "Урок 20",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_20.json"
+	},
+	{	
+		id: 21,
+		level: "A0",
+		name: "Урок 21",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_21.json"
+	},
+	{	
+		id: 22,
+		level: "A0",
+		name: "Урок 22",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_22.json"
+	},
+	{	
+		id: 23,
+		level: "A0",
+		name: "Урок 23",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_23.json"
+	},
+	{	
+		id: 151,
+		level: "B1",
+		name: "Урок 151",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_151.json"
+	},
+	{	
+		id: 152,
+		level: "B1",
+		name: "Урок 152",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_152.json"
+	},
+	{	
+		id: 153,
+		level: "B1",
+		name: "Урок 153",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_153.json"
+	},
+	{	
+		id: 154,
+		level: "B1",
+		name: "Урок 154",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_154.json"
+	},
+	{	
+		id: 155,
+		level: "B1",
+		name: "Урок 155",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_155.json"
+	},
+	{	
+		id: 156,
+		level: "B1",
+		name: "Урок 156",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_156.json"
+	},
+	{	
+		id: 157,
+		level: "B1",
+		name: "Урок 157",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_157.json"
+	},
+	{	
+		id: 158,
+		level: "B1",
+		name: "Урок 158",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_158.json"
+	},
+	{	
+		id: 251,
+		level: "C1",
+		name: "Урок 251",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_251.json"
+	},
+	{	
+		id: 252,
+		level: "C1",
+		name: "Урок 252",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_252.json"
+	},
+	{	
+		id: 253,
+		level: "C1",
+		name: "Урок 253",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_253.json"
+	},
+	{	
+		id: 254,
+		level: "C1",
+		name: "Урок 254",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_254.json"
+	},
+	{	
+		id: 255,
+		level: "C1",
+		name: "Урок 255",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_255.json"
+	},
+	{	
+		id: 256,
+		level: "C1",
+		name: "Урок 256",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_256.json"
+	},
+	{	
+		id: 257,
+		level: "C1",
+		name: "Урок 257",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_257.json"
+	},
+	{	
+		id: 258,
+		level: "C1",
+		name: "Урок 258",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_258.json"
+	}
+]
+
+export const english_galaxy_A0 = [
 	{	
 		id: 1,
 		name: "Урок 1",
@@ -113,85 +377,5 @@ export const english_galaxy = [
 		id: 23,
 		name: "Урок 23",
 		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_23.json"
-	},
-	{	
-		id: 151,
-		name: "Урок 151",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_151.json"
-	},
-	{	
-		id: 152,
-		name: "Урок 152",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_152.json"
-	},
-	{	
-		id: 153,
-		name: "Урок 153",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_153.json"
-	},
-	{	
-		id: 154,
-		name: "Урок 154",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_154.json"
-	},
-	{	
-		id: 155,
-		name: "Урок 155",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_155.json"
-	},
-	{	
-		id: 156,
-		name: "Урок 156",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_156.json"
-	},
-	{	
-		id: 157,
-		name: "Урок 157",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_157.json"
-	},
-	{	
-		id: 158,
-		name: "Урок 158",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/B1/lesson_158.json"
-	},
-	{	
-		id: 251,
-		name: "Урок 251",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_251.json"
-	},
-	{	
-		id: 252,
-		name: "Урок 252",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_252.json"
-	},
-	{	
-		id: 253,
-		name: "Урок 253",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_253.json"
-	},
-	{	
-		id: 254,
-		name: "Урок 254",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_254.json"
-	},
-	{	
-		id: 255,
-		name: "Урок 255",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_255.json"
-	},
-	{	
-		id: 256,
-		name: "Урок 256",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_256.json"
-	},
-	{	
-		id: 257,
-		name: "Урок 257",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_257.json"
-	},
-	{	
-		id: 258,
-		name: "Урок 258",
-		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/C1/lesson_258.json"
 	}
 ]
