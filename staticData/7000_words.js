84a44c9b19c22a8f1674880029b24d21113d7986
IlyaDevN <nevedrov.87@gmail.com>
2024-09-20 20:29:19 +0300

    refactor: Switch off my_sentences, del 7000_words

--- staticData/7000_words.js
+++
@@ -1,7 +0,0 @@
-export const words = [
-	{	
-		id: 1,
-		name: "Слова 0-50",
-		address: "https://ilyadevn.github.io/JsonApi/7000_words/0_50.json",
-	}
-]
--- staticData/index.js
+++ staticData/index.js
@@ -1,11 +1,9 @@
 import { english_galaxy } from "./english_galaxy";
 import { orange_playlist } from "./orange_playlist";
-import { my_sentences } from "./my_sentences";
-// import { words } from "./7000_words";
+// import { my_sentences } from "./my_sentences";
 
 export const TASKS = {
   english_galaxy,
   orange_playlist,
-  my_sentences,
-//   words
+//   my_sentences
 };
