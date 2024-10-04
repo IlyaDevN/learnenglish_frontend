4d5c8f797418d9b6f1f3a74adf7512d118b0d0e8
IlyaDevN <nevedrov.87@gmail.com>
2024-10-04 13:44:24 +0300

    refactor: Add lesson 25

--- staticData/english_galaxy.js
+++ staticData/english_galaxy.js
@@ -171,6 +171,12 @@ export const english_galaxy = [
 		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_24.json"
 	},
 	{	
+		id: 25,
+		level: "A0",
+		name: "Урок 25",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_25.json"
+	},
+	{	
 		id: 151,
 		level: "B1",
 		name: "Урок 151",
