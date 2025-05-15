ebe3b55cd46fbcb5d1c255c9419e5d8cbdfed380
IlyaDevN <nevedrov.87@gmail.com>
2025-05-15 16:02:34 +0300

    refactor: Put fetch from register to api.js

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,6 +1,7 @@
 import { Source_Sans_3 } from "next/font/google";
 import { UiButton } from "../components/ui/UiButton";
 import { useRouter } from "next/router";
+import { sendRegistrationRequest } from "../utils/api";
 import InputBlock from "../components/ui/InputBlock";
 import Link from "next/link";
 import clsx from "clsx";
@@ -18,18 +19,18 @@ export default function Register() {
         PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
     };
 
-    function formHandler(event) {
+    async function formHandler(event) {
         event.preventDefault();
         const form = document.forms.registerForm;
         const data = {
-            username: form.elements.user_name.value,
-            email: form.elements.user_email.value,
-            password: form.elements.user_password.value,
+            username: form.elements.register_name.value,
+            email: form.elements.register_email.value,
+            password: form.elements.register_password.value,
             first_name: "",
             last_name: "",
         };
 
-        const passwordConfirm = form.elements.user_password_confirm.value;
+        const passwordConfirm = form.elements.register_password_confirm.value;
 
         if (!REGEXP.NAME.test(data.username)) {
             alert("Имя некорректное");
@@ -48,39 +49,15 @@ export default function Register() {
             return;
         }
 
-        sendForm(data);
-    }
-
-    async function sendForm(data) {
-        try {
-            const response = await fetch(
-                "https://learnenglish.pp.ua/api/register/",
-                {
-                    method: "post",
-                    headers: {
-                        "Content-type": "application/json",
-                    },
-                    body: JSON.stringify(data),
-                },
-            );
-
-            const result = await response.json();
-
-            if (response.status === 201) {
-                alert(`${result.username}, your registration complete!`);
-                document.forms.registerForm.reset();
-                router.push("/");
-                return;
-            }
-            if (response.status === 205) {
-                alert("Этот e-mail уже зарегистрирован");
-                return;
-            }
-            alert("Упс, что-то пошло не так");
-        } catch (error) {
-            console.error("Fetch error:", error);
-            alert("An error occurred while communicating with the server.");
-        }
+		try {
+			const isSucceed = await sendRegistrationRequest(data);	
+			if(isSucceed) {
+				document.forms.registerForm.reset();
+				router.push("/");
+			}
+		} catch (error) {
+			alert(error.message || "Не удалось войти в систему."); 
+		}
     }
 
     return (
--- utils/api.js
+++ utils/api.js
@@ -3,15 +3,15 @@
 const API_BASE_URL = "https://learnenglish.pp.ua/api/";
 
 const saveAuthToken = (token) => {
-    localStorage.setItem('authToken', token);
+    localStorage.setItem("authToken", token);
 };
 
 const getAuthToken = () => {
-    return localStorage.getItem('authToken');
+    return localStorage.getItem("authToken");
 };
 
 const removeAuthToken = () => {
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
 };
 
 const fetchWithAuth = async (url, options = {}) => {
@@ -19,7 +19,7 @@ const fetchWithAuth = async (url, options = {}) => {
     if (authToken) {
         options.headers = {
             ...options.headers,
-            "Authorization": `Token ${authToken}`,
+            Authorization: `Token ${authToken}`,
         };
     }
     return fetch(url, options);
@@ -54,15 +54,54 @@ export async function sendLoginRequest(data) {
     }
 }
 
-export async function sendTranslationLog(data) {
+export async function sendRegistrationRequest(data) {
     try {
-        const response = await fetchWithAuth(`${API_BASE_URL}log_translation/`, { // Используем fetchWithAuth
+        const response = await fetch(`${API_BASE_URL}register/`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(data),
         });
+        const result = await response.json();
+
+        if (response.ok) {
+            const token = result.token;
+            if (token) {
+                saveAuthToken(token);
+            } else {
+                throw new Error("Токен не получен при логине");
+            }
+
+            if (response.status === 201) {
+                alert(`${result.username}, регистрация прошла успешно.`);
+                return true;
+            }
+        } else if (result.error == "Username or email already exists.") {
+            alert("Это имя или e-mail уже существует.");
+            return false;
+        } else {
+            alert("Упс, что-то пошло не так.");
+            return false;
+        }
+    } catch (error) {
+        console.error("Fetch error:", error);
+        alert("Ошибка при попытке связи с сервером.");
+    }
+}
+
+export async function sendTranslationLog(data) {
+    try {
+        const response = await fetchWithAuth(
+            `${API_BASE_URL}log_translation/`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            },
+        );
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
@@ -79,7 +118,7 @@ export async function sendTranslationLog(data) {
 export async function checkAuth() {
     try {
         const response = await fetchWithAuth(`${API_BASE_URL}check-auth/`); // Используем fetchWithAuth
-		const data = await response.json();
+        const data = await response.json();
 
         if (response.ok) {
             return data;
