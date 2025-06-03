848b200e8a25a0413a9b802095d9a62c91153d5e
IlyaDevN <nevedrov.87@gmail.com>
2025-06-03 22:21:41 +0300

    style: Format indents

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,14 +1,16 @@
 import clsx from "clsx";
 
-export default function Counter({value, className, loading, bg}) {
-
-	return (
-		<div className={clsx("flex justify-center items-center border-4 rounded-full border-yellow-400 text-xl text-yellow-900",
-			loading ? "animate-ping" : "animate-none",
-			bg ? bg : "bg-white",
-			className
-		)}>
-			{value}
-		</div>
-	)
+export default function Counter({ value, className, loading, bg }) {
+    return (
+        <div
+            className={clsx(
+                "flex justify-center items-center border-4 rounded-full border-yellow-400 text-xl text-yellow-900",
+                loading ? "animate-ping" : "animate-none",
+                bg ? bg : "bg-white",
+                className,
+            )}
+        >
+            {value}
+        </div>
+    );
 }
--- components/ui/TimerButtonsBlock.jsx
+++ components/ui/TimerButtonsBlock.jsx
@@ -1,140 +1,152 @@
 import Counter from "../Counter";
 import { UiButton } from "./UiButton";
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from "react";
 import NoSleep from "nosleep.js";
 
-export default function TimerButtonBlock({ nextSentence, showTranslation, isDataAvailable, playSoundQuestion, isSoundOn }) {
+export default function TimerButtonBlock({
+    nextSentence,
+    showTranslation,
+    isDataAvailable,
+    playSoundQuestion,
+    isSoundOn,
+}) {
+    const [timerTimeout, setTimerTimeout] = useState(18);
+    const [timerCount, setTimerCount] = useState(timerTimeout);
+    const [timeInterval, setTimeInterval] = useState(null);
+    const [isStarted, setIsStarted] = useState(false);
+    const [sentenceChangeState, setSentenceChangeState] = useState(true);
+    const [translationChangeState, setTranslationChangeState] = useState(true);
+    const [color, setColor] = useState("");
+    const isInitialMountSentence = useRef(true);
+    const isInitialTranslation = useRef(true);
 
-  const [timerTimeout, setTimerTimeout] = useState(18);
-  const [timerCount, setTimerCount] = useState(timerTimeout);
-  const [timeInterval, setTimeInterval] = useState(null);
-  const [isStarted, setIsStarted] = useState(false);
-  const [sentenceChangeState, setSentenceChangeState] = useState(true);
-  const [translationChangeState, setTranslationChangeState] = useState(true);
-  const [color, setColor] = useState("");
-  const isInitialMountSentence = useRef(true);
-  const isInitialTranslation = useRef(true);
+    useEffect(() => {
+        if (isInitialMountSentence.current) {
+            isInitialMountSentence.current = false;
+            return;
+        }
+        nextSentence();
+    }, [sentenceChangeState]);
 
-  useEffect(()=> {
-	if(isInitialMountSentence.current) {
-		isInitialMountSentence.current = false;
-		return;
-	} 
-	nextSentence();
-  }, [sentenceChangeState])
+    useEffect(() => {
+        if (isInitialTranslation.current) {
+            isInitialTranslation.current = false;
+            return;
+        }
+        showTranslation();
+    }, [translationChangeState]);
 
-  useEffect(()=> {
-	if(isInitialTranslation.current) {
-		isInitialTranslation.current = false;
-		return;
-	} 
-	showTranslation();
-  }, [translationChangeState])
+    useEffect(() => {
+        resetTimer();
+        setTimerCount(timerTimeout);
+    }, [timerTimeout]);
 
-  useEffect(()=> {
-	resetTimer();
-	setTimerCount(timerTimeout);
-  },[timerTimeout])
+    useEffect(() => {
+        let isEnableNoSleep = false;
+        const noSleep = new NoSleep();
+        document.addEventListener(
+            `click`,
+            function enableNoSleep() {
+                document.removeEventListener(`click`, enableNoSleep, false);
+                noSleep.enable();
+                isEnableNoSleep = true;
+            },
+            false,
+        );
+        return () => {
+            if (isEnableNoSleep) {
+                noSleep.disable();
+            }
+        };
+    }, []);
 
-  useEffect(() => {
-    let isEnableNoSleep = false;
-    const noSleep = new NoSleep();
-    document.addEventListener(
-      `click`,
-      function enableNoSleep() {
-        document.removeEventListener(`click`, enableNoSleep, false);
-        noSleep.enable();
-        isEnableNoSleep = true;
-      },
-      false
-    );
-    return () => {
-      if (isEnableNoSleep) {
-        noSleep.disable();
-      }
+    const startTimer = () => {
+        if (!isDataAvailable) {
+            alert("Выберите уровень, а затем урок");
+            return;
+        }
+
+        if (isStarted) {
+            return;
+        }
+
+        setIsStarted(true);
+
+        if (isSoundOn) {
+            playSoundQuestion();
+        }
+
+        setTimeInterval(
+            setInterval(() => {
+                setTimerCount((prev) => {
+                    if (prev == 5) {
+                        setTranslationChangeState((old) => !old);
+                    }
+                    if (prev == 0) {
+                        setSentenceChangeState((old) => !old);
+                        resetTimer();
+                    }
+                    return prev - 1;
+                });
+            }, 1000),
+        );
     };
-  }, []);
 
-  const startTimer = () => {
-	if(!isDataAvailable) {
-		alert("Выберите уровень, а затем урок");
-		return;
-	}
+    //   const pauseTimer = () => {
+    //     clearInterval(timeInterval);
+    //   };
 
-	if(isStarted) {
-		return;
-	}
+    const resetTimer = () => {
+        setTimerCount(timerTimeout);
+        clearInterval(timeInterval);
+        setIsStarted(false);
+    };
 
-	setIsStarted(true);
+    function increaseTime() {
+        if (timerTimeout == 60) {
+            setColor("bg-gradient-to-br from-light_blue to-dark_blue");
+            setTimeout(() => {
+                setColor("");
+            }, 200);
+            return;
+        }
+        setTimerTimeout((oldValue) => oldValue + 1);
+    }
 
-	if(isSoundOn) {
-		playSoundQuestion();
-	}
+    function decreaseTime() {
+        if (timerTimeout == 10) {
+            setColor("bg-gradient-to-br from-light_blue to-dark_blue");
+            setTimeout(() => {
+                setColor("");
+            }, 200);
+            return;
+        }
+        setTimerTimeout((oldValue) => oldValue - 1);
+    }
 
-    setTimeInterval(
-      setInterval(() => {
-        setTimerCount((prev) => {
-          if(prev == 5) {
-			setTranslationChangeState((old) => !old);
-		  } 
-		  if (prev == 0) {
-			setSentenceChangeState((old) => !old);
-            resetTimer();
-          } 
-		  return prev - 1;
-        });
-      }, 1000),
+    return (
+        <div className="flex gap-4 flex-col">
+            <div className="flex justify-between">
+                <UiButton className="min-w-[100px]" onClick={startTimer}>
+                    Старт
+                </UiButton>
+                {/* <UiButton onClick={pauseTimer}>Pause</UiButton> */}
+                <Counter value={timerCount} className={"w-14"} bg={color} />
+                <UiButton className="min-w-[100px]" onClick={resetTimer}>
+                    Стоп
+                </UiButton>
+            </div>
+            <div className="flex justify-between">
+                <UiButton className={"w-14"} onClick={decreaseTime}>
+                    -
+                </UiButton>
+                <div className="flex justify-center items-center bg-white px-4 border-4 rounded-full border-yellow-400 text-xl text-yellow-900 uppercase">
+                    Таймер
+                </div>
+                <UiButton className={"w-14"} onClick={increaseTime}>
+                    +
+                </UiButton>
+            </div>
+        </div>
     );
-  };
-
-//   const pauseTimer = () => {
-//     clearInterval(timeInterval);
-//   };
-
-  const resetTimer = () => {
-    setTimerCount(timerTimeout);
-    clearInterval(timeInterval);
-	setIsStarted(false);
-  };
-
-  function increaseTime() {
-	if(timerTimeout == 60) {
-		setColor("bg-gradient-to-br from-light_blue to-dark_blue");
-		setTimeout(()=> {
-			setColor("");
-		}, 200)
-		return;
-	}
-	setTimerTimeout((oldValue) => oldValue + 1);
-  }
-
-  function decreaseTime() {
-	if(timerTimeout == 10) {
-		setColor("bg-gradient-to-br from-light_blue to-dark_blue");
-		setTimeout(()=> {
-			setColor("");
-		}, 200)
-		return;
-	}
-	setTimerTimeout((oldValue) => oldValue - 1);
-  }
-
-  return (
-	<div className="flex gap-4 flex-col">
-		<div className="flex justify-between">
-			<UiButton className="min-w-[100px]" onClick={startTimer}>Старт</UiButton>
-			{/* <UiButton onClick={pauseTimer}>Pause</UiButton> */}
-			<Counter value={timerCount} className={"w-14"}  bg={color}/>
-			<UiButton className="min-w-[100px]" onClick={resetTimer}>Стоп</UiButton>
-    	</div>
-		<div className="flex justify-between"> 
-			<UiButton className={"w-14"} onClick={decreaseTime}>-</UiButton>
-			<div className="flex justify-center items-center bg-white px-4 border-4 rounded-full border-yellow-400 text-xl text-yellow-900 uppercase" >
-				Таймер
-			</div>
-			<UiButton className={"w-14"} onClick={increaseTime}>+</UiButton>
-		</div> 
-	</div>
-    
-  );
 }
