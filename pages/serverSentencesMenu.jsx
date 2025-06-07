c7b9190091ab27a9ce98a4c6c74cf07616eeaebd
IlyaDevN <nevedrov.87@gmail.com>
2025-06-07 13:11:30 +0300

    fix: Fix font in serverSentencesMenu

--- pages/serverSentencesMenu.jsx
+++ pages/serverSentencesMenu.jsx
@@ -1,4 +1,3 @@
-import clsx from "clsx";
 import { UiButton } from "../components/ui/UiButton";
 import { useRouter } from "next/router";
 import { TASKS } from "../staticData";
@@ -32,12 +31,7 @@ export default function ServerSentencesMenu() {
                 />
             </Head>
             <div className="px-4">
-                <div
-                    className={clsx(
-                        sourceSans3.className,
-                        "w-full max-w-4xl mx-auto bg-orange-100 border-4 border-s-gray-100 rounded-2xl px-3.5 py-8 bg-opacity-80",
-                    )}
-                >
+                <div className="w-full max-w-4xl mx-auto bg-orange-100 border-4 border-s-gray-100 rounded-2xl px-3.5 py-8 bg-opacity-80">
                     <div className="flex flex-col items-center gap-5">
                         <p className="text-2xl font-black text-yellow-900 uppercase">
                             выберите задание
