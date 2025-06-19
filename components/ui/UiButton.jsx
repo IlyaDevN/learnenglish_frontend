51f7cecf3da45817daa0ecd574ff3e7878b136ec
IlyaDevN <nevedrov.87@gmail.com>
2025-06-19 19:31:16 +0300

    style: Change UiButton style

--- components/ui/UiButton.jsx
+++ components/ui/UiButton.jsx
@@ -4,7 +4,7 @@ export function UiButton({ children, className, type, onClick }) {
   return (
     <button
       className={clsx(
-        "h-14 px-4 bg-lime-500 border-4 border-yellow-400 rounded-[50px] text-xl leading-tight text-white font-black uppercase",
+        "h-14 px-4 bg-gradient-to-b from-[#97CE4E] to-[#58AA2C] shadow-[inset_0_25px_0_0_rgba(255,255,255,0.1)] border-4 border-yellow-400 rounded-[50px] text-xl leading-tight text-white font-black uppercase",
         className,
       )}
 	  type={type}
