7c129242cc5c45f19835007f9e062b0599a908c9
IlyaDevN <nevedrov.87@gmail.com>
2025-06-20 05:40:24 +0300

    style: Use colors from tailwind.config in UiButton

--- components/ui/UiButton.jsx
+++ components/ui/UiButton.jsx
@@ -1,16 +1,23 @@
 import clsx from "clsx";
 
 export function UiButton({ children, className, type, onClick }) {
-  return (
-    <button
-      className={clsx(
-        "h-14 px-4 bg-gradient-to-b from-[#97CE4E] to-[#58AA2C] shadow-[inset_0_25px_0_0_rgba(255,255,255,0.1)] border-4 border-yellow-400 rounded-[50px] text-xl leading-tight text-white font-black uppercase",
-        className,
-      )}
-	  type={type}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+    const hasCustomBg = className && (
+		className.includes('bg-') ||
+		className.includes('from-') ||
+		className.includes('to-')
+  	);
+
+    return (
+        <button
+            className={clsx(
+                "h-14 px-4 shadow-[inset_0_25px_0_0_rgba(255,255,255,0.1)] border-4 border-yellow-400 rounded-[50px] text-xl leading-tight text-white font-black uppercase",
+                !hasCustomBg && "bg-gradient-to-b from-light_green to-dark_green",
+				className,
+            )}
+            type={type}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
 }
