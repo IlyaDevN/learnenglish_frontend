d808a20b52be4a123107c71426b3e980dcff52f3
IlyaDevN <nevedrov.87@gmail.com>
2025-06-22 06:24:27 +0300

    style: Chnage font to bold

--- components/ui/ContentField.jsx
+++ components/ui/ContentField.jsx
@@ -4,7 +4,7 @@ export function ContentField({ children, className }) {
   return (
     <div
       className={clsx(
-        "relative text-lg text-yellow-900 pr-14 pl-3.5 py-3 border-2 rounded-lg border-yellow-900 bg-white bg-opacity-50 shadow-lg min-h-[84px]",
+        "relative text-lg font-bold text-yellow-900 pr-14 pl-3.5 py-3 border-2 rounded-lg border-yellow-900 bg-white bg-opacity-50 shadow-lg min-h-[84px]",
         className,
       )}
     >
