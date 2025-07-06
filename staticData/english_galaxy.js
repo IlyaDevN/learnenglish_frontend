3909f8529d4b74b04c452016bb66e19206e8469c
IlyaDevN <nevedrov.87@gmail.com>
2025-07-06 10:27:40 +0300

    feat: Add lesson 48

--- staticData/english_galaxy.js
+++ staticData/english_galaxy.js
@@ -288,6 +288,12 @@ export const english_galaxy = [
 		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_47.json"
 	},
 	{	
+		id: 48,
+		level: "A0",
+		name: "Урок 48",
+		address: "https://ilyadevn.github.io/JsonApi/english_galaxy/A0/lesson_48.json"
+	},
+	{	
 		id: 4000,
 		level: "B1",
 		name: "Mix",
